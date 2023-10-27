52ced15b21a4a30ae949fb55a1369d4a259374bd
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-26 23:54:07 -0500

    Implementar arreglo de pokeneas

---
+++ src/models/Pokeneas.js
@@ -0,0 +1,68 @@
+const Pokeneas = [
+  {
+    id: 1,
+    nombre: "Programador nea",
+    altura: 1.75,
+    habilidades: ["Programar"],
+    imagen: "vinculo a la imagen",
+    frase:
+      "Si ustedes son programadores y ya ganan plata, busquen otra fuente de ingresos",
+    video: "N/A",
+  },
+  {
+    id: 2,
+    nombre: "El fuicioso",
+    altura: 1.8,
+    habilidades: ["Ser del verde, ser feliz"],
+    imagen: "vinculo a la imagen",
+    frase: "Sabe que? Todo bien todo bonito, solo Nacional a morir",
+    video: "https://www.youtube.com/watch?v=SHjSqQ5r6To",
+  },
+  {
+    id: 3,
+    nombre: "Mono Yin Yon",
+    altura: 1.6,
+    habilidades: ["Geografía"],
+    imagen: "vinculo a la imagen",
+    frase:
+      "¿La capital de montabesh? Mmm no se pai, nada nea estoy crochis porque tuki tuki muñeco al alma, como yo no salgo de aquí de Medellín itagui, poray al otro pai lado tal vez sí, pero cómo estoy aquí es crochis hermano",
+    video: "https://www.youtube.com/watch?v=bKT58OfMS8w",
+  },
+  {
+    id: 4,
+    nombre: "Ferxxo",
+    altura: 1.72,
+    habilidades: ["Cantar"],
+    imagen: "vinculo a la imagen",
+    frase: "La noche tenía más estrellas que Boca",
+    video: "N/A",
+  },
+  {
+    id: 5,
+    nombre: "Toreto",
+    altura: 1.8,
+    habilidades: ["Exladrón"],
+    imagen: "vinculo a la imagen",
+    frase: "Soy un pedagogo re educativo",
+    video: "https://www.youtube.com/watch?v=nu7hZ0nfSlo",
+  },
+  {
+    id: 6,
+    nombre: "Proceso de paz",
+    altura: 1.75,
+    habilidades: ["Astronomía"],
+    imagen: "vinculo a la imagen",
+    frase:
+      "Llos cielos son azules, las nubes son blancas, la luna es blanca, las estrellas brillan con mas fuerza, nuestra estrella natal llamada sol sigue brillando para nosotros a través de todo el tiempo que ha transcurrido en el universo. Además de eso se han conformado nuevas galaxias una llamada adromeda que es muy lejana a nosotros y también está la vida láctea",
+    video: "https://www.youtube.com/watch?v=lwcmFNm_Eos",
+  },
+  {
+    id: 7,
+    nombre: "Luis Eduardo",
+    altura: 1.2,
+    habilidades: ["Conducción"],
+    imagen: "vinculo a la imagen",
+    frase: "Ya empezó a patear",
+    video: "https://www.youtube.com/watch?v=ITrFyOp3_Vg",
+  },
+];
