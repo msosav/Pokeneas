677a4a055988cab753e9b85260ac752669979bf6
Miguel Sosa <miguelsosa2003@gmail.com>
2023-11-04 21:20:58 -0500

    Actualizar archivos a clases

--- src/controllers/Api/PokeneasApiController.js
+++ src/controllers/Api/PokeneasApiController.js
@@ -1,20 +1,22 @@
 const { Pokeneas } = require("../../models/Pokeneas");
 const os = require("os");
 
-const index = (req, res) => {
-  const number = Math.floor(Math.random() * Pokeneas.length);
-  const pokenea = Pokeneas[number];
+class PokeneasApiController {
+  static index = (req, res) => {
+    const number = Math.floor(Math.random() * Pokeneas.length);
+    const pokenea = Pokeneas[number];
 
-  const { id, name, height, skills } = pokenea;
+    const { id, name, height, skills } = pokenea;
 
-  res.render('pokeneas/show_json', {
-    id,
-    name,
-    height,
-    skills,
-    "docker_container": os.hostname(),
-  });
-};
+    res.render("pokeneas/show_json", {
+      id,
+      name,
+      height,
+      skills,
+      docker_container: os.hostname(),
+    });
+  };
+}
 
 module.exports = {
   index,
--- src/controllers/PokeneasController.js
+++ src/controllers/PokeneasController.js
@@ -1,28 +1,24 @@
 const { Pokeneas } = require("../models/Pokeneas");
 const os = require("os");
 
-const show_card = (req, res) => {
+class PokeneasController {
+  static index = (req, res) => {
+    res.render("pokeneas/home");
+  };
 
-  const number = Math.floor(Math.random() * Pokeneas.length);
-  const pokenea = Pokeneas[number];
+  static show_card = (req, res) => {
+    const number = Math.floor(Math.random() * Pokeneas.length);
+    const pokenea = Pokeneas[number];
 
-  const { name, image, quote } = pokenea;
+    const { name, image, quote } = pokenea;
 
-  res.render('pokeneas/show_card', {
-    name,
-    image,
-    quote,
-    "docker_container": os.hostname(),
-  });
-  
+    res.render("pokeneas/show_card", {
+      name,
+      image,
+      quote,
+      docker_container: os.hostname(),
+    });
+  };
+}
 
-};
-
-const index = (req, res) => {
-    res.render('pokeneas/home');
-};
-
-module.exports = {
-  index,
-  show_card,
-};
+module.exports = PokeneasController;
