a23f62cc98fe9e9e83c3ee15cf8e700e6ec25591
EsteTruji <etrujilloc@eafit.edu.co>
2023-11-04 20:16:03 -0500

    feature/especificacion-rutas

--- Index.js
+++ Index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const ApiRoutes = require("./src/routes/ApiRoutes");
+const app = express();
+const port = 80;
+app.set("view engine", "ejs");
 
-class Index {
-  static main() {
-    const app = express();
-    const port = 80;
-    app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("./src/public"));
 
-    app.use("/api", ApiRoutes.initialize());
+app.use("/", require("./src/routes/web"));
+app.use("/show_pokecard", require("./src/routes/web"));
+app.use("/show_pokejson", require("./src/routes/web"));
 
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  }
-}
-
-Index.main();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
